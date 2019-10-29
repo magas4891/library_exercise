3b64e515098268eff2a9d8ece139a11d111e245f
magas4891 <magasishe@gmail.com>
2019-10-29 18:31:42 +0200

    g Book scaffold

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,3 @@
 class Book
   include Mongoid::Document
-  include Mongoid::Timestamps
-  field :name, type: String
-  field :description, type: Text
-  field :like_counter, type: Integer
-  field :author, type: String
 end
