28c4eb75ee5199c43ac52a4cab0a9a3d7550a99d
magas4891 <magasishe@gmail.com>
2019-10-30 17:18:07 +0200

    books index

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,14 @@
 class Book
   include Mongoid::Document
+  include Mongoid::Timestamps
+  field :name, type: String
+  field :cover, type: String
+  field :description, type: String
+  field :likes_counter, type: Integer, default: -> { 0 }
+  field :author, type: String
+  field :comment, type: String
+  field :status, type: Boolean, default: -> { true }
+
+  mount_uploader :cover, CoverUploader
+
 end
