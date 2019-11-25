c25dd3d9f7afbafd9933449cd26c3604fb75325a
magas4891 <magasishe@gmail.com>
2019-11-25 17:39:08 +0200

    book show refactoring

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+
   def index
   end
 
   def create
-    @book = Book.find(params[:id])
+    @book = Book.find(params[:book_id])
     @comment = @book.comments.new(comments_params)
-    @comment.user = current_user
-    @comment.commenter =
-        current_user.nick? ? current_user.nick : current_user.name
+    @comment.user_id = current_user.id
+    @comment.book_id = @book.id
+    @comment.commenter = current_user.name
     if @comment.save
       redirect_to book_path(@book)
     end
@@ -18,6 +19,6 @@ class CommentsController < ApplicationController
   private
 
   def comments_params
-    params.require(:comment).permit(:commenter, :text, :user_id, :book_id)
+    params.require(:comment).permit(:commenter, :comment, :user_id, :book_id)
   end
 end
